70b643d57fe36a8fe3dfbe071e9579d16d803b96
mbmybook <mbmybook.com@gmail.com>
2016-10-30 18:24:07 -0700

    set homepage

--- app/app.js
+++ app/app.js
@@ -1,8 +1,17 @@
+'use strict';
+
 var express = require('express');
+//var path = require('path');
+//var angular = require('angular');
 var app = express();
 
+//angular.module('website');
+
+//app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/home.html');
 });
 
 app.listen(80);
