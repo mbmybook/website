a96dc1b16242e787e3e1ed5aa66dbc52336ce739
mbmybook <mbmybook.com@gmail.com>
2016-11-10 23:02:11 -0800

    Add pdf viewers, joomag, yudu, and fliphtml5

--- app/public/routes.js
+++ app/public/routes.js
@@ -8,6 +8,18 @@
         templateUrl: 'public/html/index.html',
         controller: 'MainCtrl',
       })
+			.when('/joomag', {
+				templateUrl: 'public/html/joomag.html',
+				controller: 'MainCtrl',
+			})
+			.when('/yudu', {
+				templateUrl: 'public/html/yudu.html',
+				controller: 'MainCtrl',
+			})
+			.when('/fliphtml5', {
+				templateUrl: 'public/html/fliphtml5.html',
+				controller: 'MainCtrl',
+			})
       .otherwise({redirectTo : '/coming-soon'});
   }]);
 })();
