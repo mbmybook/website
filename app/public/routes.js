3bf7326bf77d02aa1ad7d4c6be74a11cdd8e5448
mbmybook <mbmybook.com@gmail.com>
2016-11-11 17:55:25 -0800

    Add more way to read pdf

--- app/public/routes.js
+++ app/public/routes.js
@@ -20,6 +20,18 @@
 				templateUrl: 'public/html/fliphtml5.html',
 				controller: 'MainCtrl',
 			})
+			.when('/googlepdf', {
+				templateUrl: 'public/html/googlepdf.html',
+				controller: 'MainCtrl',
+			})
+			.when('/embedpdf', {
+				templateUrl: 'public/html/embedpdf.html',
+				controller: 'MainCtrl',
+			})
+			.when('/googledrive', {
+				templateUrl: 'public/html/googledrive.html',
+				controller: 'MainCtrl',
+			})
       .otherwise({redirectTo : '/coming-soon'});
   }]);
 })();
--- app/server.js
+++ app/server.js
@@ -5,5 +5,8 @@
   var app = express();
   app.use(express.static('./'));
 
+	app.get('/pdf/a08.pdf', function (req, res) {
+		res.sendFile(__dirname + '/public/a08.pdf');
+	})
   app.listen(80);
 })();
