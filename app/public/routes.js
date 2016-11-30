d451c6f1df753c81d89cd7b4402d94d9b57330e7
mbmybook <mbmybook.com@gmail.com>
2016-11-29 21:58:03 -0800

    Add about, issues, and events pages

--- app/public/routes.js
+++ app/public/routes.js
@@ -4,34 +4,50 @@
 
   app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/coming-soon', {
-        templateUrl: 'public/html/index.html',
+			.when('/', {
+				templateUrl: 'public/html/index.html',
+				controller: 'MainCtrl',
+			})
+      .when('/about', {
+        templateUrl: 'public/html/about.html',
         controller: 'MainCtrl',
       })
-			.when('/joomag', {
-				templateUrl: 'public/html/joomag.html',
-				controller: 'MainCtrl',
-			})
-			.when('/yudu', {
-				templateUrl: 'public/html/yudu.html',
-				controller: 'MainCtrl',
-			})
+      .when('/issues', {
+        templateUrl: 'public/html/issues.html',
+        controller: 'MainCtrl',
+      })
+      .when('/events', {
+        templateUrl: 'public/html/events.html',
+        controller: 'MainCtrl',
+      })
+      .when('/coming-soon', {
+        templateUrl: 'public/html/coming-soon.html',
+        controller: 'MainCtrl',
+      })
 			.when('/fliphtml5', {
 				templateUrl: 'public/html/fliphtml5.html',
-				controller: 'MainCtrl',
-			})
-			.when('/googlepdf', {
-				templateUrl: 'public/html/googlepdf.html',
-				controller: 'MainCtrl',
-			})
-			.when('/embedpdf', {
-				templateUrl: 'public/html/embedpdf.html',
 				controller: 'MainCtrl',
 			})
 			.when('/googledrive', {
 				templateUrl: 'public/html/googledrive.html',
 				controller: 'MainCtrl',
 			})
-      .otherwise({redirectTo : '/coming-soon'});
+			// .when('/joomag', {
+			// 	templateUrl: 'public/html/joomag.html',
+			// 	controller: 'MainCtrl',
+			// })
+			// .when('/yudu', {
+			// 	templateUrl: 'public/html/yudu.html',
+			// 	controller: 'MainCtrl',
+			// })
+			// .when('/googlepdf', {
+			// 	templateUrl: 'public/html/googlepdf.html',
+			// 	controller: 'MainCtrl',
+			// })
+			// .when('/embedpdf', {
+			// 	templateUrl: 'public/html/embedpdf.html',
+			// 	controller: 'MainCtrl',
+			// })
+      .otherwise({redirectTo : '/'});
   }]);
 })();
