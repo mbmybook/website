cc3381f09d6b385a37580f04afb5138d4c3616aa
mbmybook <mbmybook.com@gmail.com>
2016-12-28 10:40:54 -0800

    add new event content (jpg rip)

--- app/src/components/EventNew.js
+++ app/src/components/EventNew.js
@@ -3,11 +3,19 @@ import '../static/css/style.css'
 import '../static/css/event.css'
 
 import React from 'react'
+import Picture from './Picture'
 
 class EventNew extends React.Component {
   render() {
     return (
-      <div>hi</div>
+      <div className='event-new-panel horizontal-center debug'>
+        <Picture src={require('../static/img/event-new/0816-MB_P_82-1.jpg')}/>
+        <Picture src={require('../static/img/event-new/0816-MB_P_83-1.jpg')}/>
+        <Picture src={require('../static/img/event-new/0816-MB_P_84NEW-1.jpg')}/>
+        <Picture src={require('../static/img/event-new/0816-MB_P_85-1.jpg')}/>
+        <Picture src={require('../static/img/event-new/0816-MB_P_86-1.jpg')}/>
+        <Picture src={require('../static/img/event-new/0816-MB_P_87-1.jpg')}/>
+      </div>
     )
   }
 }
