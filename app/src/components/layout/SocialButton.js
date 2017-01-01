f5a629a7fcbc581a501fbf576bd04029382f4407
mbmybook <mbmybook.com@gmail.com>
2016-12-31 23:49:41 -0800

    - fix email phone too long - mobile wechat and facebook open different links

--- app/src/components/layout/SocialButton.js
+++ app/src/components/layout/SocialButton.js
@@ -9,9 +9,18 @@ import ModalButton from './ModalButton'
 class SocialButton extends React.Component {
   render() {
     if (this.props.item.type === 'link') {
-      return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
+      if (window.screen.width > 800) {
+        return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
+      } else {
+        return <LinkButton icon={this.props.item.icon} link={this.props.item.deep_link}/>
+      }
+      
     } else if (this.props.item.type === 'modal') {
-      return <ModalButton icon={this.props.item.icon} image={this.props.item.image}/>
+      if (window.screen.width > 800) {
+        return <ModalButton icon={this.props.item.icon} image={this.props.item.image}/>
+      } else {
+        return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
+      }
     } else {
       // handle error
       return <div />
