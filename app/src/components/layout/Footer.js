82b2a49da784e3dbc6a730e5d487810fd53996b1
mbmybook <mbmybook.com@gmail.com>
2017-01-01 01:11:13 -0800

    fix footer issue on mobile

--- app/src/components/layout/Footer.js
+++ app/src/components/layout/Footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
   render() {
     return <div className='footer-container debug'>
       <div className='footer horizontal-center debug'>
-        <div className='contact-panel font-helvetica font-size-20-12 vertical-center debug'>
+        <div className='contact-panel font-helvetica font-size-20-15 vertical-center debug'>
           <a href='tel:604-518-6668'>604-518-6668</a>
           <a href='mailto:&#105;&#110;&#102;&#111;&#064;&#109;&#098;&#109;&#121;&#098;&#111;&#111;&#107;&#046;&#099;&#097;'>&#105;&#110;&#102;&#111;&#064;&#109;&#098;&#109;&#121;&#098;&#111;&#111;&#107;&#046;&#099;&#097;</a>
         </div>
--- app/src/components/layout/SocialButton.js
+++ app/src/components/layout/SocialButton.js
@@ -9,17 +9,16 @@ import ModalButton from './ModalButton'
 class SocialButton extends React.Component {
   render() {
     if (this.props.item.type === 'link') {
-      if (window.screen.width > 800) {
-        return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
-      } else {
+      if (/Mobi/i.test(navigator.userAgent)) {
         return <LinkButton icon={this.props.item.icon} link={this.props.item.deep_link}/>
-      }
-      
-    } else if (this.props.item.type === 'modal') {
-      if (window.screen.width > 800) {
-        return <ModalButton icon={this.props.item.icon} image={this.props.item.image}/>
       } else {
         return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
+      }
+    } else if (this.props.item.type === 'modal') {
+      if (/Mobi/i.test(navigator.userAgent)) {
+        return <LinkButton icon={this.props.item.icon} link={this.props.item.link}/>
+      } else {
+        return <ModalButton icon={this.props.item.icon} image={this.props.item.image}/>
       }
     } else {
       // handle error
