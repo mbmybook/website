bfec73756c577e4f955722c053e34f7fd26dff95
mbmybook <mbmybook.com@gmail.com>
2017-04-07 00:10:19 -0700

    Add distrubition content

--- app/src/components/distribution/Distribution.js
+++ app/src/components/distribution/Distribution.js
@@ -4,6 +4,7 @@ import './distribution.css'
 
 import React from 'react'
 import Helmet from 'react-helmet'
+import list from './distributionList.json'
 
 class Distribution extends React.Component {
   constructor(props) {
@@ -12,8 +13,22 @@ class Distribution extends React.Component {
   
   render() {
     return (
-      <div>
+      <div className='distribution-panel horizontal-center debug'>
         <Helmet title='mb. MyBook - 派發地點' />
+        <h1 className='distribution-title font-helvetica font-size-36'>全城派發</h1>
+        {list.map(city =>
+          <div key={city.city}>
+            <h1 className='city-title font-helvetica font-size-24'>{city.city}</h1>
+            <div className='city-locations'>
+              {city.locations.map(location =>
+                <div key={location.address} className='location-panel col-xs-6'>
+                  <p className='location-name font-helvetica font-size-15'>{location.name}</p>
+                  <p className='location-address font-helvetica font-size-15'>{location.address}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     )
   }
