9286dc53f2891415311e02dce4ae2ae7b7b82d8e
mbmybook <mbmybook.com@gmail.com>
2017-05-03 21:27:54 -0700

    When there is no event in the section simply hide the section

--- app/src/components/events/EventMetaSection.js
+++ app/src/components/events/EventMetaSection.js
@@ -8,22 +8,22 @@ import NavLink from '../shared/NavLink'
 
 class EventMetaSection extends React.Component {
   render() {
-    return (
-      <div className='event-meta-section debug'>
-        <h1 className='event-meta-section-title font-helvetica font-size-18'>{this.props.title}</h1>
-        {this.props.events.length <= 0 ? (
-          <p className='event-meta-section-placeholder font-helvetica font-size-13'>{this.props.placeholder}</p>
-        ) : (
-          this.props.events.map(event => 
+    if (this.props.events.length <= 0) {
+      return <div></div>
+    } else {
+      return (
+        <div className='event-meta-section debug'>
+          <h1 className='event-meta-section-title font-helvetica font-size-18'>{this.props.title}</h1>
+          {this.props.events.map(event => 
             <NavLink className='event-meta' key={event.slug} to={'/events/' + event.slug}>
               <p className='event-meta-title font-helvetica font-size-18'>{event.post ? event.post.title : event.slug}</p>
               <p className='event-meta-meta font-helvetica font-size-13'>{event.duration}</p>
               <p className='event-meta-meta font-helvetica font-size-13'>{event.location}</p>
             </NavLink>
-          )
-        )}
-      </div>
-    )
+          )}
+        </div>
+      )
+    }
   }
 }
 
--- app/src/components/events/EventPreviewSection.js
+++ app/src/components/events/EventPreviewSection.js
@@ -8,18 +8,18 @@ import EventPreview from './EventPreview'
 
 class EventPreviewSection extends React.Component {
   render() {
-    return (
-      <div className='event-preview-section'>
-        <h1 className='event-preview-section-title font-helvetica font-size-18'>{this.props.title}</h1>
-        {this.props.events.length <= 0 ? (
-          <p className='event-preview-section-placeholder font-helvetica font-size-13'>{this.props.placeholder}</p>
-        ) : (
-          this.props.events.map(event => 
+    if (this.props.events.length <= 0) {
+      return <div></div>
+    } else {
+      return (
+        <div className='event-preview-section'>
+          <h1 className='event-preview-section-title font-helvetica font-size-18'>{this.props.title}</h1>
+          {this.props.events.map(event => 
             event.post && <EventPreview key={event.slug} slug={event.post.slug} title={event.post.title} duration={event.duration} location={event.location} excerpt={event.post.excerpt} featured_image={event.post.featured_image} />
-          )
-        )}
-      </div>
-    )
+          )}
+        </div>
+      )
+    }
   }
 }
 
